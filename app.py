3e0b6435f72f8a8f74577a28e8a94aa1db8f1406
Bai  Xue <xb@Bais-MacBook-Pro.local>
2020-03-15 14:55:37 -0400

    make changes to app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+# from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
